0f0d1d6ec542c54e3c718e0c60b209f46e66c946
abbas nasser <37267949+abbasnasser@users.noreply.github.com>
2023-04-27 23:48:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ let x = document.getElementById("body");
 let gallery = document.createElement("div");
 gallery.id = "gallery";
 gallery.className = "gallery";
-x.classList.add('tempCenter')
+x.classList.add("tempCenter");
 x.appendChild(gallery);
 let gallerydoc = document.getElementById("gallery");
 let loadimg = new Image();
-loadimg.src = 'loadinglogo.jpg';
+loadimg.src = "loadinglogo.jpg";
 loadimg.classList = "loading";
-gallerydoc.appendChild(loadimg)
-
+gallerydoc.appendChild(loadimg);
 
 let imgss = [];
-var new_data = [];
-var new_data_sorted = [];
+let bool_last_image = false;
 
+//dont delete -----------------
 main = [];
 list = [];
 ss = [];
 spare_ss = [];
+//--------------------
 
-function addgame(imgsrc, gamenametxt, gameranktxt) {
+function addgame(imgsrc, gamenametxt, gameranktxt, state) {
   let container = document.createElement("div");
-  container.id = "container";
+  container.id = state;
   container.className = "container";
   let container2r = document.createElement("div");
   container2r.id = "container2";
@@ -32,8 +32,6 @@ function addgame(imgsrc, gamenametxt, gameranktxt) {
   gamemedal.classList = "medal";
   let gamename = document.createElement("div");
   gamename.textContent = gamenametxt;
-  console.log(gamenametxt.length)
-  gameNameWithoutSpaces = gamenametxt.replace(/\s/g, '');
   gamename.className = "gamename";
   let gamerank = document.createElement("div");
   gamerank.textContent = gameranktxt;
@@ -48,11 +46,30 @@ function addgame(imgsrc, gamenametxt, gameranktxt) {
 }
 
 function addGamesToPage() {
-  for (var i = 0; i < ss.length; i++) {
-    addgame(ss[i][2], ss[i][1], ss[i][0]);
+  expanded_elements = 0;
+  length_of_list = ss.length / 4;
+  //just adding the number of rank as id to the card for further uses
+  while (length_of_list % 1 != 0) {
+    length_of_list -= 0.25;
+    expanded_elements += 1;
   }
-}
 
+  if (expanded_elements == 1) {
+    ll = ss.length;
+    for (var i = 0; i < ss.length - 1; i++) {
+      addgame(ss[i][2], ss[i][1], ss[i][0], i + 1);
+    }
+
+    addgame(ss[ll - 1][2], ss[ll - 1][1], ss[ll - 1][0], ll);
+  } else {
+    ll = ss.length;
+    for (var i = 0; i < ss.length; i++) {
+      addgame(ss[i][2], ss[i][1], ss[i][0], i);
+    }
+  }
+
+  //----------------------------//
+}
 
 function search() {
   var search = document.getElementById("search");
@@ -80,7 +97,13 @@ function start() {
   request.onload = function () {
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
-      body.innerHTML +=
+      let x = document.getElementById("body");
+      let search_container = document.createElement("div");
+      search_container.id = "search_container";
+      search_container.className = "search_container";
+
+      x.appendChild(search_container);
+      search_container.innerHTML +=
         ' <div class="searchBox" ><input id="search" class="search" placeholder="Search" type="search" /></div>';
 
       main = data.slice(0);
@@ -90,7 +113,7 @@ function start() {
       addGamesToPage();
       gg = document.getElementById("gallery");
       gg.innerHTML = "";
-      x.classList.remove('tempCenter')
+      x.classList.remove("tempCenter");
       for (let i = 0; i < imgss.length; i++) {
         gg.appendChild(imgss[i][1]);
       }
@@ -154,3 +177,13 @@ function update_new_data(list) {
     ss.push([ll[i][0], ll[i][1], img]);
   }
 }
+
+window.addEventListener("resize", function () {
+  var viewport_width = window.innerWidth;
+
+  if (viewport_width < 300) {
+    loop();
+  } else if (viewport_width > 300 ) {
+    loop();
+  }
+});
