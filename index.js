6c38b212414ad0dfe66841a2b7761bc2842bc61f
abbas nasser <37267949+abbasnasser@users.noreply.github.com>
2023-06-13 06:03:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,189 +1,140 @@
-let x = document.getElementById("body");
-let gallery = document.createElement("div");
-gallery.id = "gallery";
-gallery.className = "gallery";
-x.classList.add("tempCenter");
-x.appendChild(gallery);
-let gallerydoc = document.getElementById("gallery");
-let loadimg = new Image();
-loadimg.src = "loadinglogo.jpg";
-loadimg.classList = "loading";
-gallerydoc.appendChild(loadimg);
-
-let imgss = [];
-let bool_last_image = false;
-
-//dont delete -----------------
-main = [];
-list = [];
-ss = [];
-spare_ss = [];
-//--------------------
-
-function addgame(imgsrc, gamenametxt, gameranktxt, state) {
-  let container = document.createElement("div");
-  container.id = state;
-  container.className = "container";
-  let container2r = document.createElement("div");
-  container2r.id = "container2";
-  container2r.className = "container_rank_name";
-  let gamemedal = new Image();
-  gamemedal.src = "medal.png";
-  gamemedal.classList = "medal";
-  let gamename = document.createElement("div");
-  gamename.textContent = gamenametxt;
-  gamename.className = "gamename";
-  let gamerank = document.createElement("div");
-  gamerank.textContent = gameranktxt;
-  gamerank.className = "gamerank";
-  container.appendChild(imgsrc);
-  container2r.appendChild(gamename);
-  container2r.appendChild(gamemedal);
-  container2r.appendChild(gamerank);
-  container.appendChild(container2r);
-  imgss.push([gamenametxt, container]);
-  spare_ss.push([gamenametxt, container]);
+var listOfdata = [];
+function main() {
+  var body = document.getElementById("b");
+  body.classList.add("temp_body");
+  var loading = document.createElement("img");
+  loading.id = "loading";
+  loading.classList.add("loading");
+  loading.src = "loading.gif";
+  body.appendChild(loading);
+  getData();
 }
+main();
 
-function addGamesToPage() {
-  expanded_elements = 0;
-  length_of_list = ss.length / 4;
-  //just adding the number of rank as id to the card for further uses
-  while (length_of_list % 1 != 0) {
-    length_of_list -= 0.25;
-    expanded_elements += 1;
-  }
-
-  if (expanded_elements == 1) {
-    ll = ss.length;
-    for (var i = 0; i < ss.length - 1; i++) {
-      addgame(ss[i][2], ss[i][1], ss[i][0], i + 1);
-    }
-
-    addgame(ss[ll - 1][2], ss[ll - 1][1], ss[ll - 1][0], ll);
-  } else {
-    ll = ss.length;
-    for (var i = 0; i < ss.length; i++) {
-      addgame(ss[i][2], ss[i][1], ss[i][0], i);
-    }
-  }
-
-  //----------------------------//
-}
-
-function search() {
-  var search = document.getElementById("search");
-  search.addEventListener("input", (event) => {
-    filterList = spare_ss.filter((element) =>
-      element[0].toLowerCase().includes(search.value.toLowerCase())
-    );
-    imgss = filterList;
-    gg = document.getElementById("gallery");
-    gg.innerHTML = "";
-    for (let i = 0; i < imgss.length; i++) {
-      gg.appendChild(imgss[i][1]);
-    }
-    loop();
-  });
-}
-
-function start() {
+function getData() {
   var request = new XMLHttpRequest();
   request.open(
     "GET",
     "https://script.google.com/macros/s/AKfycbzBe6xVYo-CJVN8AAtg-bbmBbP6jDxbAliMoZCFknvJW7KvOQMZguB5LW6qim-YmWIX/exec",
     true
   );
-  request.onload = function () {
-    var data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-      let x = document.getElementById("body");
-      let search_container = document.createElement("div");
-      search_container.id = "search_container";
-      search_container.className = "search_container";
-
-      x.appendChild(search_container);
-      search_container.innerHTML +=
-        ' <div class="searchBox" ><input id="search" class="search" placeholder="Search" type="search" /></div>';
-
-      main = data.slice(0);
-      main.shift();
-      list = main.slice(0);
-      update_new_data(list);
-      addGamesToPage();
-      gg = document.getElementById("gallery");
-      gg.innerHTML = "";
-      x.classList.remove("tempCenter");
-      for (let i = 0; i < imgss.length; i++) {
-        gg.appendChild(imgss[i][1]);
+  try {
+    request.onload = async function () {
+      var data = JSON.parse(this.response);
+      if (request.status >= 200 && request.status < 400) {
+        listOfdata = data;
+        search();
+        var body = document.getElementById("b");
+        var loading = document.getElementById("loading");
+        body.classList.remove("temp_body");
+        body.removeChild(loading);
+        addSekeleton(data);
+        var search_d = document.getElementById("search");
+        search_d.style.visibility = "visible";
+      } else if (request.status == 403) {
+        console.log("error");
       }
-
-      search();
-      loop();
-      window.addEventListener("scroll", function () {
-        loop();
-      });
-    } else {
-      console.log("error");
-    }
-  };
-
-  request.send();
+    };
+    request.send();
+  } catch {
+    console.log("nointernet");
+  }
+}
+function addSekeleton(data) {
+  for (i = 0; i < data.length; i++) {
+    generateSuggestion_for_Search(data[i][1]);
+    newCard(data[i][2], data[i][0], data[i][1], i);
+  }
 }
 
-start();
+function newCard(imglink, rank, gamename, index) {
+  var gallery = document.getElementById("gallery");
+  //1
+  var card_container = document.createElement("div");
+  card_container.classList.add("card_container");
+  card_container.id = index + 1 + "roll";
 
-function loop() {
-  var elementsToShow = document.querySelectorAll(".container");
-  elementsToShow.forEach(function (element) {
-    if (isElementInViewport(element)) {
-      element.classList.add("showContainer");
-    } else {
-      element.classList.remove("showContainer");
+  var image_holder = document.createElement("div");
+  image_holder.id = index + "img";
+  //2
+  var main_image = document.createElement("img");
+  main_image.classList.add("image");
+  main_image.src = imglink;
+
+  //3
+  var rank_div = document.createElement("div");
+  rank_div.classList.add("rank_div");
+  rank_div.id = index + "rank_div";
+  //4
+  var empty = document.createElement("div");
+  //5
+  var medal = document.createElement("img");
+  medal.classList.add("medal");
+  medal.src = "./medal.png";
+  //6
+  var skel = document.createElement("img");
+  skel.src = "skeleton.gif";
+  var number = document.createElement("div");
+  number.classList.add("number");
+  number.textContent = rank;
+  //7
+  var game_name = document.createElement("div");
+  game_name.classList.add("game_name");
+  game_name.textContent = gamename;
+
+  //8 sekeleton
+
+  var skeleton_form = document.createElement("img");
+  skeleton_form.classList.add("image");
+  skeleton_form.src = "skeleton.gif";
+  skeleton_form.id = index;
+  image_holder.appendChild(skeleton_form);
+  //
+  card_container.appendChild(image_holder);
+  card_container.appendChild(rank_div);
+
+  gallery.appendChild(card_container);
+
+  //adding
+  try {
+    main_image.onload = function () {
+      var gal = document.getElementById(index + "img");
+      var removeImage = document.getElementById(index);
+      var rank_div_get = document.getElementById(index + "rank_div");
+
+      gal.removeChild(removeImage);
+
+      gal.appendChild(main_image);
+      empty.appendChild(medal);
+      empty.appendChild(number);
+      rank_div_get.appendChild(empty);
+      rank_div_get.appendChild(game_name);
+    };
+  } catch {
+    console.log("error abbas");
+  }
+}
+
+function search() {
+  var search = document.getElementById("search");
+  search.addEventListener("input", (event) => {
+    var vla = search.value.toLowerCase();
+    for (i = 0; i < listOfdata.length; i++) {
+      if (vla == listOfdata[i][1].toLowerCase()) {
+        var tt = listOfdata[i][0] - 1;
+        var location = "#" + tt + "roll";
+        console.log(location);
+
+        window.location.hash = location;
+      }
     }
   });
-
-  scroll(loop);
 }
 
-function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0 && rect.bottom >= 0) ||
-    (rect.bottom >=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <=
-        (window.innerHeight || document.documentElement.clientHeight)) ||
-    (rect.top >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight))
-  );
+function generateSuggestion_for_Search(word) {
+  var suggestions = document.getElementById("suggestions");
+  var option = document.createElement("option");
+  var atrib = document.createElement("a");
+  option.value = word;
+  suggestions.appendChild(option);
 }
-
-function update_new_data(list) {
-  for (var i = 0; i < list.length; i++) {
-    let gameimg = new Image();
-    gameimg.src = list[i][2];
-    gameimg.classList = "gameimg";
-    gameimg.loading = "lazy";
-    gameimg.onload = add(gameimg, list);
-  }
-
-  function add(img, ll) {
-    ss.push([ll[i][0], ll[i][1], img]);
-  }
-}
-
-window.addEventListener("resize", function () {
-  var viewport_width = window.innerWidth;
-
-  if (viewport_width < 300) {
-    loop();
-  } else if (viewport_width > 300 ) {
-    loop();
-  }
-});
